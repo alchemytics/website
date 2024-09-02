6c3b49fac458db40b57d9e0375a8c86a2a699688
wol4aravio <panovskiy.v@yandex.ru>
2024-09-02 20:18:48 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://alchemytics.ru",
+  integrations: [tailwind(), mdx(), sitemap()]
+});
