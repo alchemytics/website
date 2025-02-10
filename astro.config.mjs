67796ca670ae9f38549118b04b7cac990bf46eeb
wol4aravio <panovskiy.v@yandex.ru>
2025-02-10 20:48:07 +0300

    refresh app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import alpinejs from '@astrojs/alpinejs';
+import node from '@astrojs/node';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://alchemytics.ru",
+  integrations: [alpinejs(), mdx(), sitemap()],
+  adapter: node({
+    mode: 'standalone'
+  }),
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
